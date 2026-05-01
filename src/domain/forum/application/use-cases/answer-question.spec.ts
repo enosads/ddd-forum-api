cbb5a5d344c268fd6a547740e47049d1ce51b5b0
Enos Andrade <enosads@gmail.com>
2026-04-30 23:12:10 -0300

    refactor: reorganize domain structure following DDD patterns
    - Move entities to src/domain/forum/enterprise/entities
    - Move repositories to src/domain/forum/application/repositories
    - Move use cases to src/domain/forum/application/use-cases
    - Implement layered architecture with clear separation of concerns

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,4 +1,4 @@
-import type { Answer } from "../entities/answer";
+import type { Answer } from "../forum/enterprise/entities/answer";
 import { AnswerQuestionUseCase } from "./answer-question";
 
 const fakeAnswersRepository = {
