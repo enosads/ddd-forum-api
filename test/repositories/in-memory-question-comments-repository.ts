74f0fc08d8e057527dd41c7350420e86c411fcf6
Enos Andrade <enosads@gmail.com>
2026-08-12 23:02:57 -0300

    feat: implement delete question comment use case

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -6,7 +6,21 @@ export class InMemoryQuestionCommentsRepository
 {
   public questionComments: QuestionComment[] = [];
 
+  async findById(id: string): Promise<QuestionComment | null> {
+    const answer = this.questionComments.find(item => item.id.value === id);
+    return answer ?? null;
+  }
+
   async create(questionComment: QuestionComment) {
     this.questionComments.push(questionComment);
+  }
+
+  async delete(questionComment: QuestionComment): Promise<void> {
+    const index = this.questionComments.findIndex(
+      item => item.id.value === questionComment.id.value
+    );
+    if (index !== -1) {
+      this.questionComments.splice(index, 1);
+    }
   }
 }
