b2ae90a75b6ea595aa3b2d32c336c5b2300b2d26
Enos Andrade <enosads@gmail.com>
2026-08-12 23:32:04 -0300

    feat: implement fetch answer comments use case

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,7 +1,12 @@
+import type { PaginationParams } from "@/core/repositories/pagination-params";
 import type { AnswerComment } from "../../enterprise/entities/answer-comment";
 
 export interface AnswerCommentsRepository {
   findById: (id: string) => Promise<AnswerComment | null>;
+  findManyByAnswerId: (
+    questionId: string,
+    params: PaginationParams
+  ) => Promise<AnswerComment[]>;
   create: (AnswerComment: AnswerComment) => Promise<void>;
   delete: (AnswerComment: AnswerComment) => Promise<void>;
 }
